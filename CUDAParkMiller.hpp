458eacdbaf7edfefb166b88daf6ef8795cd4c418
Christian Fobel <christian@fobel.net>
2012-10-31 11:31:12 -0400

    Templatize the CUDAParkMiller class, fix types

--- CUDAParkMiller.hpp
+++ CUDAParkMiller.hpp
@@ -1,41 +1,48 @@
 #ifndef ___CUDA_PARK_MILLER__HPP___
 #define ___CUDA_PARK_MILLER__HPP___
 
+template <class int_t, class real_t>
 class CUDAParkMiller {
-typedef double real_t;
 protected:
-    static real_t const a = 16807;      //ie 7**5
-    static real_t const m = 2147483647; //ie 2**31-1
-    real_t const reciprocal_m;
-    real_t const reciprocal_m_sub_1;
-    unsigned int seed;
+    static int_t const a = 16807;      //ie 7**5
+    static int_t const m = 2147483647; //ie 2**31-1
+    real_t reciprocal_m;
+    real_t reciprocal_m_sub_1;
+    int_t seed;
 public:
-    __device__ CUDAParkMiller(unsigned int seed) : seed(seed),
-        reciprocal_m(1.0 / m), reciprocal_m_sub_1(1.0 / (m - 1)) {}
+    __device__ CUDAParkMiller() : seed(0) {
+        reciprocal_m = 1.0 / (float)m;
+        reciprocal_m_sub_1 = 1.0 / (float)(m - 1);
+    }
 
-    __device__ void set_seed(unsigned int i_seed) {
+    __device__ CUDAParkMiller(int_t seed) : seed(seed) {
+        reciprocal_m = 1.0 / (float)m;
+        reciprocal_m_sub_1 = 1.0 / (float)(m - 1);
+    }
+
+    __device__ void set_seed(int_t i_seed) {
         seed = i_seed;
     }
 
-    __device__ int MOD(real_t value, real_t divisor, real_t divisor_inv) {
-        return value - floor(value * divisor_inv) * divisor;
+    __device__ int_t MOD(real_t value, real_t divisor, real_t divisor_inv) {
+        return value - (int_t)floor(value * divisor_inv) * divisor;
     }
 
-    __device__ int get_seed() {
+    __device__ int_t current_value() {
         return seed;
     }
 
-    __device__ int get_value() {
+    __device__ int_t get_value() {
         real_t temp = seed * a;
         seed = MOD(temp, m, reciprocal_m);
         return seed;
     }
 
-    __device__ int rand_int(int max_value) {
+    __device__ int_t rand_int(int_t max_value) {
         return get_value() % (max_value + 1);
     }
 
-    __device__ real_t rand_double() {
+    __device__ real_t rand_real() {
         return get_value() * reciprocal_m_sub_1;
     }
 };
